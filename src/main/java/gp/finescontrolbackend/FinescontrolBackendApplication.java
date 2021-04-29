7e0c3f3832308a6bc7e840782fbd33b4ea94b7a8
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-04-29 15:59:51 -0300

    Adicionando projeto. [ADD]

---
+++ src/main/java/gp/finescontrolbackend/FinescontrolBackendApplication.java
@@ -0,0 +1,13 @@
+package gp.finescontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinescontrolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinescontrolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gp/finescontrolbackend/FinescontrolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gp.finescontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinescontrolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
