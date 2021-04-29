23182c52b3f0db37e7697fb5a16475fecf498d42
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-04-29 16:10:41 -0300

    Adicionando 'dtos'. [ADD]
    Classes responsáveis por receber e trafegar os dados entre o frontend e o backend.

---
+++ src/main/java/gp/finescontrolbackend/dtos/FineDTO.java
@@ -0,0 +1,29 @@
+package gp.finescontrolbackend.dtos;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class FineDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String aitCode;
+    private Instant moment;
+    private Date dueDate;
+    private Date paymentDate;
+    private Boolean identifiedDriver;
+    private Double amount;
+    private CarDTO car;
+    private DriverDTO driver;
+    private InfractionDTO infraction;
+}
